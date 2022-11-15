7cbdc616ba0a51591af373d9cdd14c9227fee245
Rahul Rajesh Sonar <96967994+sonar288@users.noreply.github.com>
2022-11-15 14:33:50 +0530

    uploded

---
+++ index.js
@@ -0,0 +1,10 @@
+const navbar= document.querySelector("nav");
+const openMenu= document.getElementById("menu-button");
+const closeMenu= document.getElementById("menu-closed");
+
+ openMenu.addEventListener("click",() =>{
+   navbar.classList.add("open");
+ })
+ closeMenu.addEventListener("click",() =>{
+   navbar.classList.remove("open");
+ })
